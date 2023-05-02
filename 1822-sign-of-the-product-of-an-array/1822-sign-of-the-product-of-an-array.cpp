d04cc5ea116229d9755131cb65ba29c2b1487743
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-05-02 16:39:20 +0530

    Time: 8 ms (27.40%), Space: 10.2 MB (45.94%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int arraySign(vector<int>& nums) {
-     int countNegative=0;
+     double prod=1;
         for(int i=0;i<nums.size();i++){
-         if(nums[i]<0)countNegative++;
-        else if(nums[i]==0) return 0; 
-     }
-        if(countNegative%2!=0){
-            return -1;
-        }
-        return 1;}
+          prod*=nums[i];}
+        if(prod>0)return 1;
+        else if(prod<0)return -1;
+        else return 0;}
 };
