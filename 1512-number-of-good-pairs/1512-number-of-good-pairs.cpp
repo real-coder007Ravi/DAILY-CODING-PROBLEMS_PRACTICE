1ad5822270c61a7720027ca21d60bbe7e12cc63b
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-02 02:14:19 +0530

    Time: 4 ms (32.98%), Space: 7.2 MB (26.94%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        //VARIABLE NAMING VERY IMPORTANT AND DRY RUN BEFORE THE SOLUTION VERY VERY IMPORTANT
-    int CountOfIdenticalPairs=0;
-for(int FirstElementIndex=0;FirstElementIndex<nums.size();FirstElementIndex++){
-for(int SecondElementIndex=FirstElementIndex+1;SecondElementIndex<nums.size();SecondElementIndex++){
-                if(nums[FirstElementIndex]==nums[SecondElementIndex]){
-                    CountOfIdenticalPairs++;}
-                        
-                 }
-             }
-           return CountOfIdenticalPairs;
+    int res=0;
+    unordered_map<int,int>Map;
+    for (int a: nums) {
+       res += Map[a];
+        Map[a]++;
     }
+      return res;
+}
+        
+        
+        
+        
+        
+        
+        
+//         //VARIABLE NAMING VERY IMPORTANT AND DRY RUN BEFORE THE SOLUTION VERY VERY IMPORTANT
+//     int CountOfIdenticalPairs=0;
+// for(int FirstElementIndex=0;FirstElementIndex<nums.size();FirstElementIndex++){
+// for(int SecondElementIndex=FirstElementIndex+1;SecondElementIndex<nums.size();SecondElementIndex++){
+//                 if(nums[FirstElementIndex]==nums[SecondElementIndex]){
+//                     CountOfIdenticalPairs++;}
+                        
+//                  }
+//              }
+//            return CountOfIdenticalPairs;
+//     }
+    
+      
 };
