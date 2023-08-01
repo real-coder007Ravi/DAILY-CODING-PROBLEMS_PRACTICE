84f0708c0b4a20015c82c663c9342a3ac52a6e74
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-01 23:36:14 +0530

    Time: 3 ms (34.46%), Space: 5.9 MB (99.59%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-           vector<double>ans;
-             double kelvin=celsius+273.15;
-             double fahrenheit=(celsius*1.80)+32.00;
-             ans.push_back(kelvin);
-             ans.push_back(fahrenheit);
-        return ans;
+        //Brute Force  
+        vector<double>ConvertedTempValues;
+        double KelvinTemp=celsius+273.15;
+        double FahrenheitTemp=celsius *  1.80 + 32.00;
+        ConvertedTempValues.push_back(KelvinTemp);
+        ConvertedTempValues.push_back(FahrenheitTemp);
+        return ConvertedTempValues;
     }
 };
