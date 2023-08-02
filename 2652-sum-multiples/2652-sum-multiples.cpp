1e8e3472c8b456bcf66da730d6ac082115d1a9fc
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-03 02:22:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.44%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.cpp
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
-    int sumOfMultiples(int n) {
-        int SumOfAllMultiples=0;
-        for(int startingNumber=1;startingNumber<=n;startingNumber++){
-            if(startingNumber%3==0 ||startingNumber%5==0||startingNumber%7==0){
-                SumOfAllMultiples+=startingNumber;
-            }
+    // int sumOfMultiples(int n) {
+    //     int SumOfAllMultiples=0;
+    //     for(int startingNumber=1;startingNumber<=n;startingNumber++){
+    //         if(startingNumber%3==0 ||startingNumber%5==0||startingNumber%7==0){
+    //             SumOfAllMultiples+=startingNumber;
+    //         }
+    //    }
+    //     return SumOfAllMultiples;
+    // 
+        int CalculateSumOfAllFcatorsOnKUptoN(int n,int k){
+            return k*(n/k)*(n/k +1)/2;
+        }
+      int sumOfMultiples(int n) {
+          return CalculateSumOfAllFcatorsOnKUptoN(n,3)+ CalculateSumOfAllFcatorsOnKUptoN(n,5)+ CalculateSumOfAllFcatorsOnKUptoN(n,7)- CalculateSumOfAllFcatorsOnKUptoN(n,3*5)- CalculateSumOfAllFcatorsOnKUptoN(n,5*7)- CalculateSumOfAllFcatorsOnKUptoN(n,7*3)+ CalculateSumOfAllFcatorsOnKUptoN(n,3*5*7);  
        }
-        return SumOfAllMultiples;
-    }
+    
 };
