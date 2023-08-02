196bfc682638151e2ea1f0e6264e57e11a986494
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-03 02:38:52 +0530

    Time: 2 ms (40.97%), Space: 6 MB (29.63%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int xorOperation(int n, int start) {
-         int *nums=new int[n];
         int res=0;
         for(int i=0;i<n;i++){
-              nums[i]=start+2*i;
-              res^=nums[i];
+             res^= start+2*i;    
         }
         return res;
         
