25f674fd999bccc80712effae483ce0c950ece3b
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-03 02:02:38 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.76%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.cpp
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int sumOfMultiples(int n) {
-        int sum=0;
-        for(int i=1;i<=n;i++){
-          if(i%3==0||i%5==0||i%7==0){
-             sum+=i;}
-         
-        }
-        return sum;
+        int SumOfAllMultiples=0;
+        for(int startingNumber=1;startingNumber<=n;startingNumber++){
+            if(startingNumber%3==0 ||startingNumber%5==0||startingNumber%7==0){
+                SumOfAllMultiples+=startingNumber;
+            }
+       }
+        return SumOfAllMultiples;
     }
 };
