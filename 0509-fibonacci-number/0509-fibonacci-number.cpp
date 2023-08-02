2c826bac4533d2b47f421a19eedf9c54804e66c7
Ravi Kumar Gupta <87241044+real-coder007Ravi@users.noreply.github.com>
2023-08-03 01:36:14 +0530

    Time: 0 ms (100.00%), Space: 6 MB (23.77%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -11,28 +11,29 @@ public:
 //          return dp[n];  
 //      }
 //     };
-    //MEMORIZATION
-// int helper(int n,int *arr){
-//         if(n==0||n==1){
-//             return n;
-//            }
-//          if(arr[n]!=-1){
-//              return arr[n];
-//          }
-//          int ans=helper(n-1,arr)+helper(n-2,arr);
-//          arr[n]=ans;
-//          return ans;
-// }
-// int fib(int n) {
-//          int *arr=new int[n+1];
-//          for(int i=0;i<=n;i++){
-//              arr[i]=-1;
-//          }
-//          return helper(n,arr);}
-    //BRUTE FORCE
- int fib(int n) {
+    // MEMORIZATION
+int helper(int n,int *arr){
         if(n==0||n==1){
-           return n;
-        }
-        return fib(n-1)+fib(n-2);
-    }};
+            return n;
+           }
+         if(arr[n]!=-1){
+             return arr[n];
+         }
+         int ans=helper(n-1,arr)+helper(n-2,arr);
+         arr[n]=ans;
+         return ans;
+}
+int fib(int n) {
+         int *arr=new int[n+1];
+         for(int i=0;i<=n;i++){
+             arr[i]=-1;
+         }
+         return helper(n,arr);}
+ //    //BRUTE FORCE
+ // int fib(int n) {
+ //        if(n==0||n==1){
+ //           return n;
+ //        }
+ //        return fib(n-1)+fib(n-2);
+ //    }
+};
